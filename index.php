b87f94bfca66c3c0e6980a2504c9f3324cd22a38
Arsen Asanov <tatarin08082002@gmail.com>
2025-02-25 23:47:26 +0300

    push on server

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 
-echo "hello world";
-echo "hello world";
-echo "hello world";
-echo "hello world";
+echo "hello world<br>";
+echo "hello world<br>";
+echo "hello world<br>";
+echo "hello world<br>";
